483e0b772b662553d17a000cd0260ea509f7d673
MalayaliOwner <70500048+MalayaliOwner@users.noreply.github.com>
2020-09-30 22:09:03 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -137,7 +137,7 @@ def send_start(bot, update):
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
     text = """🤖 Hey there! I am a Group Manager Bot.
 
-    ❔ If you have any questions about how to use me please give me /help..."""
+❔ If you have any questions about how to use me please give me /help..."""
 
     
 
